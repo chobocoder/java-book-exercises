fdee69231fd44d731e608a8d31f1aaaf2ccac7a4
choboplayer <narrotor@yahoo.com>
2025-01-16 22:46:45 -0500

    initial problem

---
+++ src/chapter_1/Initial.java
@@ -0,0 +1,23 @@
+package chapter_1;
+
+public class Initial {
+    /**
+     * Instructions:
+     * Write, compile, and test a class that displays your initials on the screen. Compose
+     * each initial with five lines of initials, as in the following example:
+     *          J
+     *          J
+     *          J
+     *          J
+     * J        J
+     * JJJJJJJJJJ
+     */
+
+    public static void main (String[] args) {
+        System.out.println("      J");
+        System.out.println("      J");
+        System.out.println("      J");
+        System.out.println("J     J");
+        System.out.println("JJJJJJJ");
+    }
+}
