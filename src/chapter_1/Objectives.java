38081c09f2904fe6a0b726bf055968fd4015a03e
choboplayer <narrotor@yahoo.com>
2025-01-16 22:50:48 -0500

    objectives problem

---
+++ src/chapter_1/Objectives.java
@@ -0,0 +1,21 @@
+package chapter_1;
+
+public class Objectives {
+
+    /**
+     * Instructions:
+     * Write, compile, and test a class that displays all the objectives listed at the beginning
+     * of this chapter. Save the class as Objectives.java.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("* Define basic programming terminology");
+        System.out.println("* Describe procedural and object-oriented programming concepts");
+        System.out.println("* Describe the features of the Java programming language");
+        System.out.println("* Analyze a Java application that produces console output");
+        System.out.println("* Add comments to a Java class");
+        System.out.println("* Save, compile, run, and modify a Java application");
+        System.out.println("* Create a Java application that produces GUI output");
+        System.out.println("* Correct errors and find help");
+    }
+}
